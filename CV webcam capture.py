970707a3cdaa7120642392c56b14137c0e198689
Prashant Rawat <48995196+RawatPrashant70@users.noreply.github.com>
2019-03-31 15:10:51 +0530

    Add files via upload

---
+++ CV webcam capture.py
@@ -0,0 +1,21 @@
+import cv2
+import matplotlib.pyplot as plt
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    if cap.isOpened():
+        ret, frame = cap.read()
+    else:
+        ret = False
+
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    plt.imshow(img)
+    plt.title('camera')
+    plt.show()
+    
+    cap.release()
+
+if __name__ == '__main__':
+    main()
